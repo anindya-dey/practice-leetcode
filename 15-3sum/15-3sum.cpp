ec72ef343bca3f8b0b733092ef1bd62de0f76b54
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-07 11:42:33 +0530

    Time: 123 ms (67.53%), Space: 19.9 MB (77.68%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -8,17 +8,19 @@ public:
         
         for(int i = 0; i < n - 2; i++) {
             if(i == 0 || (i > 0 && nums[i] != nums[i-1])) {
-                int a = -nums[i];
+                int a = nums[i];
                 int lo = i + 1;
                 int hi = n - 1;
                 while(lo < hi) {
-                    if((nums[lo] + nums[hi]) == a) {
-                        result.push_back({ -a, nums[lo], nums[hi] });
+                    int sum = a + nums[lo] + nums[hi];
+                    
+                    if(sum == 0) {
+                        result.push_back({ a, nums[lo], nums[hi] });
                         while(lo < hi && nums[lo] == nums[lo + 1]) lo++;
                         while(lo < hi && nums[hi] == nums[hi - 1]) hi--;
 
                         lo++; hi--;
-                    } else if((nums[lo] + nums[hi]) < a) {
+                    } else if(sum < 0) {
                         lo++;
                     } else {
                         hi--;
