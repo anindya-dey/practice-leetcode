d422ac3d43e7c0c5cdd92f41cb63d57ef6947ed5
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-12 16:33:31 +0530

    Time: 28 ms (48.37%), Space: 10.8 MB (53.99%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result(2, -1);
         unordered_map<int, int> tracker;
         int n = nums.size();
         
         for(int i = 0; i < n; i++) {
             int complement = target - nums[i];
-            if(tracker.find(complement) == tracker.end()) {
-                tracker.insert({nums[i], i});
-            } else {
-                result[0] = tracker[complement];
-                result[1] = i;
-                break;
+            if(tracker.find(complement) != tracker.end()) {
+                return { tracker[complement], i };
             }
+            
+            tracker.insert({nums[i], i});
         }
 
-        return result;
+        return { -1, -1 };
     }
 };
