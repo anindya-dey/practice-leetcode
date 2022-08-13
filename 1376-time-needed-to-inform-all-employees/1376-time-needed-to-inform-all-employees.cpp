7ae98a3653b8b2fceed1e288096470ec21e55ca6
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-13 21:41:51 +0530

    Time: 1123 ms (6.12%), Space: 90.6 MB (99.00%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        if(n == 1) return informTime[0];
+        
+        int mint = INT_MIN;
+        
+        for(int i = 0; i < n; i++) {
+            int j = manager[i], time = 0;
+            while(j != -1) {
+                time += informTime[j];
+                j = manager[j];
+            }
+            // time += informTime[headID];
+            mint = max(mint, time);
+        }
+        
+        return mint;
+    }
+};
