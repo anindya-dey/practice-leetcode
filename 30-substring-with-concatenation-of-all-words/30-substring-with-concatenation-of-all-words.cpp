af34c2b7ebba725582b4612b95c1830b4343de05
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-13 15:22:18 +0530

    Time: 287 ms (67.05%), Space: 29 MB (51.66%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -5,9 +5,8 @@ public:
         
         int length = s.length(),
             totalWords = words.size(),
-            wordSize = words[0].size();
-
-        int limit = length - totalWords * wordSize;
+            wordSize = words[0].size(),
+            limit = length - totalWords * wordSize;
         
         unordered_map<string, int> frequencyMap;
         for(string word: words) {
@@ -31,35 +30,6 @@ public:
             if(j == totalWords) ans.push_back(i);
         }
         
-        return ans;
-        
-        
-        /*
-        unordered_map<string, int> counts;
-        
-        for (string word : words)
-            counts[word]++;
-        
-        int n = s.length(), num = words.size(), len = words[0].length();
-        vector<int> indexes;
-        
-        for (int i = 0; i < n - num * len + 1; i++) {
-            unordered_map<string, int> seen;
-            
-            int j = 0;
-            for (; j < num; j++) {
-                string word = s.substr(i + j * len, len);
-                if (counts.find(word) != counts.end()) {
-                    seen[word]++;
-                    if (seen[word] > counts[word])
-                        break;
-                }
-                else break;
-            }
-        
-            if (j == num) indexes.push_back(i);
-        }
-        return indexes;
-        */
+        return ans;        
     }
 };
