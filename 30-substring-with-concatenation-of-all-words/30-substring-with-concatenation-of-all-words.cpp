8f533590c0d0ef2b92f24410e830ea0bd7e53749
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-13 15:21:00 +0530

    Time: 370 ms (57.14%), Space: 29 MB (51.66%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,48 +1,40 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        /*
-        int length = s.size();
-        int totalWords = words.size();
         vector<int> ans;
         
-        if(length == 0 || totalWords == 0) return ans;
-        
-        int wordSize = words[0].size();
+        int length = s.length(),
+            totalWords = words.size(),
+            wordSize = words[0].size();
+
         int limit = length - totalWords * wordSize;
         
         unordered_map<string, int> frequencyMap;
-        
-        for(int i = 0; i < totalWords; i++) {
-            string word = words[i];
-            
-            if(!frequencyMap[word]) frequencyMap[word] = 0;
-            
+        for(string word: words) {
             frequencyMap[word]++;
         }
         
         for(int i = 0; i <= limit; i++) {
             unordered_map<string, int> seenWords;
-            
-            for(int j = 0; j < totalWords; j++) {
+            int j = 0;
+            for(; j < totalWords; j++) {
                 int wordIndex = i + j * wordSize;
                 string subWord = s.substr(wordIndex, wordSize);
                 
                 if(!frequencyMap[subWord]) break;
                 
-                if(!seenWords[subWord]) seenWords[subWord] = 0;
-                
                 seenWords[subWord]++;
                 
                 if(seenWords[subWord] > frequencyMap[subWord]) break;
-
-                if((j + 1) == totalWords) ans.push_back(i);
             }
+            
+            if(j == totalWords) ans.push_back(i);
         }
         
         return ans;
-        */
         
+        
+        /*
         unordered_map<string, int> counts;
         
         for (string word : words)
@@ -68,5 +60,6 @@ public:
             if (j == num) indexes.push_back(i);
         }
         return indexes;
+        */
     }
 };
