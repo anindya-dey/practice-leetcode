ba76955f4ca86e45a44062ce62043814198e6b42
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-13 20:50:37 +0530

    Time: 20 ms (71.91%), Space: 10.6 MB (97.96%) - LeetHub

---
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.cpp
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(!root) return 0;
+        
+        int depth = 1;
+        for(Node *node: root->children) {
+            depth = max(depth, 1 + maxDepth(node));
+        }
+        return depth;
+    }
+};
