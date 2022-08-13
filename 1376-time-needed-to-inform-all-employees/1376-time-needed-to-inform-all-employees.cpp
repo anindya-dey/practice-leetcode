141fd8db46c5444b2be098283414b1e8016f71d6
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-13 22:00:15 +0530

    Time: 1872 ms (5.03%), Space: 90.8 MB (96.33%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -4,20 +4,14 @@ public:
         if(n == 1) return informTime[0];
         
         int mint = INT_MIN;
-        unordered_map<int, int> tracker;
         
         for(int i = 0; i < n; i++) {
             int j = manager[i], time = 0;
-            
-            if(tracker[j]) continue;
-            
             while(j != -1) {
                 time += informTime[j];
                 j = manager[j];
             }
             mint = max(mint, time);
-            
-            tracker[j] = mint;
         }
         
         return mint;
