487f6a077f7c22fdb01ea863c4446671cdb29879
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-15 12:28:52 +0530

    Time: 11 ms (77.18%), Space: 5.9 MB (97.90%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> m{
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000},
-        };
+        int m[24] = {};
+        m['I'-'A'] = 1;
+        m['V'-'A'] = 5;
+        m['X'-'A'] = 10;
+        m['L'-'A'] = 50;
+        m['C'-'A'] = 100;
+        m['D'-'A'] = 500;
+        m['M'-'A'] = 1000;
         
         int n = s.size();
         int ans = 0;
         
         for(int i = 0; i < n; i++) {
-            //int curr = m[s[i]];
-            if(((i + 1) < n) && (m[s[i]] < m[s[i+1]])) {
-                ans -= m[s[i]];
+            int curr = s[i] - 'A';
+            if(((i + 1) < n) && (m[curr] < m[s[i+1]-'A'])) {
+                ans -= m[curr];
             } else {
-                ans += m[s[i]];
+                ans += m[curr];
             }
         }
         
