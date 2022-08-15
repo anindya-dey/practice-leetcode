4202656cabbc2b5c4e310e549ae17c8d7949ea65
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-15 12:31:00 +0530

    Time: 11 ms (77.18%), Space: 5.9 MB (86.74%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,24 +1,36 @@
 class Solution {
+private:
+    int m(char c) {
+        switch(c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
 public:
     int romanToInt(string s) {
-        int m[24] = {};
-        m['I'-'A'] = 1;
-        m['V'-'A'] = 5;
-        m['X'-'A'] = 10;
-        m['L'-'A'] = 50;
-        m['C'-'A'] = 100;
-        m['D'-'A'] = 500;
-        m['M'-'A'] = 1000;
-        
         int n = s.size();
         int ans = 0;
         
         for(int i = 0; i < n; i++) {
-            int curr = s[i] - 'A';
-            if(((i + 1) < n) && (m[curr] < m[s[i+1]-'A'])) {
-                ans -= m[curr];
+            int curr = m(s[i]);
+            if(((i + 1) < n) && (curr < m(s[i+1]))) {
+                ans -= curr;
             } else {
-                ans += m[curr];
+                ans += curr;
             }
         }
         
