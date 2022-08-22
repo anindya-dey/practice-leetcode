1cf9bd1bdefb6c57a8a172a2ce394f46a3d8927c
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-22 10:44:30 +0530

    Time: 4 ms (40.16%), Space: 5.9 MB (71.30%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n <= 0) return false;
+        
+        // (n & (n-1) == 0)
+        // -- Is there only one 1?
+        //    -- e.g, 0100 (=4) or 0001 0000 (=16)
+        // Power of 4 has only one 1.
+        
+        // ((n & 0x55555555) != 0)
+        // -- If so, is the 1 in the right place?
+        //    -- e.g, 0100 (=4) and not 0010 (=2)
+        // -- This will exclude those numbers which are power of 2 but not of 4 (like 2, 8, 32, etc)
+        // -- 0x55555555 = Hex representation of 0101 0101 0101 ... 0101
+        
+        return ((n & (n-1)) == 0) && ((n & 0x55555555) != 0);
+    }
+};
