8ab65e7a4df6703fdefe109fd44f67db20756722
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-26 20:20:18 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.90%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 private:
     vector<int> findDigits(int num) {
@@ -29,6 +30,23 @@ public:
             num = num << 1;
         }
                 
+        return false;
+    }
+};
+*/
+
+class Solution {
+private:
+    long counter(int N) {
+        long res = 0;
+        for (; N; N /= 10) res += pow(10, N % 10);
+        return res;
+    }
+public:
+    bool reorderedPowerOf2(int N) {
+        long c = counter(N);
+        for (int i = 0; i < 32; i++)
+            if (counter(1 << i) == c) return true;
         return false;
     }
 };
