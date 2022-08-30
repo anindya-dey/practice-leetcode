63f6091dfef5d50ad0b324199ad5ffc3a06bf0c6
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-08-30 22:18:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.11%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -4,10 +4,10 @@ public:
         int steps = 0;
         
         while(num != 0) {
-            if((num % 2) == 0) {
-                num /= 2;
-            } else {
+            if((num & 1) == 1) {
                 num -= 1;
+            } else {
+                num = num >> 1;
             }
             steps++;
         }
