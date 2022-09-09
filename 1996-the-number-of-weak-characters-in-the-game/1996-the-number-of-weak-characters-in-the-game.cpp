772872269bcef73d8d86d213c7abc95f44c495e2
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-09-09 20:11:38 +0530

    Time: 1624 ms (6.20%), Space: 125.1 MB (91.78%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    bool static sortByAttack(vector<int> &a, vector<int> &b) {
+        if(a[0] == b[0])
+            return a[1] < b[1];
+        
+        return a[0] > b[0];
+    }
+public:
+    int numberOfWeakCharacters(vector<vector<int>>& properties) {
+        map<int, vector<vector<int>>, greater<int>> m;
+        
+        sort(properties.begin(), properties.end(), sortByAttack);
+        
+        int count = 0;
+        int n = properties.size();
+        int maxDefense = properties[0][1];
+        
+        for(int i = 1; i < n; i++) {
+            if(properties[i][1] > maxDefense) {
+                maxDefense = properties[i][1];
+            }
+            
+            if(properties[i][1] < maxDefense) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
