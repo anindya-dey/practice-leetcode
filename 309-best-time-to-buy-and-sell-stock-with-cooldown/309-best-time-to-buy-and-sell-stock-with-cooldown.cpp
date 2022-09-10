b2a55df7ef714ce65fedfca2856e2a8005db5cb2
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-09-10 20:52:42 +0530

    Time: 15 ms (19.83%), Space: 11.3 MB (77.72%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -27,6 +27,7 @@ public:
 };
 */
 
+/*
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -48,3 +49,33 @@ public:
         return dp[0][1];
     }
 };
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        
+        vector<int> ahead2(2, 0);
+        vector<int> ahead1(2, 0);
+        vector<int> curr(2, 0);
+        
+        for(int i = n-1; i >= 0; i--) {
+            curr[1] = max(
+                -prices[i] + ahead1[0],
+                ahead1[1]
+            );
+
+            curr[0] = max(
+                prices[i] + ahead2[1],
+                ahead1[0]
+            );
+            
+            ahead2 = ahead1;
+            ahead1 = curr;
+        }
+        
+        return curr[1];
+    }
+};
+
