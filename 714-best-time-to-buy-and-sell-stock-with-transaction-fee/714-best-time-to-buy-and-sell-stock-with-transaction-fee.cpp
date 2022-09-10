43ced2ca29c6fde16f876337ede754369e519f66
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-09-10 20:56:18 +0530

    Time: 239 ms (52.80%), Space: 55.1 MB (77.06%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -4,22 +4,19 @@ public:
         int n = prices.size();
         
         vector<int> ahead(2, 0);
-        vector<int> curr(2, 0);
         
         for(int i = n-1; i >= 0; i--) {
-            curr[1] = max(
+            ahead[1] = max(
                 -prices[i] + ahead[0],
                 ahead[1]
             );
 
-            curr[0] = max(
+            ahead[0] = max(
                 prices[i] - fee + ahead[1],
                 ahead[0]
             );
-            
-            ahead = curr;
         }
         
-        return curr[1];
+        return ahead[1];
     }
 };
