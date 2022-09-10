b123b43e5077b3b3bfaa0b5588dead3623e3c57f
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2022-09-10 20:47:17 +0530

    Time: 8 ms (54.20%), Space: 11.9 MB (48.17%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,3 +1,5 @@
+/** RECURSION with DP **/
+/*
 class Solution {
 private:
     int f(vector<int> &prices, int i, bool buy, vector<vector<int>> &dp) {
@@ -21,5 +23,28 @@ public:
     int maxProfit(vector<int>& prices) {
         vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
         return f(prices, 0, true, dp);
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+2, vector<int>(2, 0));
+        
+        for(int i = n-1; i >= 0; i--) {
+            dp[i][1] = max(
+                -prices[i] + dp[i+1][0],
+                dp[i+1][1]
+            );
+
+            dp[i][0] = max(
+                prices[i] + dp[i+2][1],
+                dp[i+1][0]
+            );
+        }
+        
+        return dp[0][1];
     }
 };
