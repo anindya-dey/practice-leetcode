70fd7414e5b871bfdddd6bc5edfbafb922827755
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2023-06-23 11:16:51 +0530

    Time: 4 ms (83.60%), Space: 8.8 MB (35.33%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,16 +1,37 @@
-class Solution(object):
-    def arithmeticTriplets(self, nums, diff):
-        """
-        :type nums: List[int]
-        :type diff: int
-        :rtype: int
-        """
-        count = 0
-        s = set(nums)
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        // int i = 0, j = 1, k = 2, count = 0, n = nums.size();
+        // for(int i = 0; i < n - 2; i++) {
+        //     for(int j = i + 1; j < n -1; j++) {
+        //         for(int k = j + 1; k < n; k++) {
+        //             if(((nums[j] - nums[i]) == diff) && ((nums[k] - nums[j]) == diff)) {
+        //                 count++;
+        //             }
+        //         }
+        //     }
+        // }
         
-        for num in nums:
-            if ((num - diff) in s) and ((num + diff) in s):
-                count += 1
+
+        // int count = 0;
+        // unordered_map<int, bool> m = {};
+        // for(int i = 0; i < nums.size(); i++) {
+        //     m[nums[i]] = true;
+        // }
+        // for(int i = 0; i < nums.size(); i++) {
+        //     count += m[nums[i] - diff] && m[nums[i] + diff];
+        // }
+        // return count;
         
-        return count
         
+        int count = 0;
+        unordered_set<int> s = {};
+        for(int i = 0; i < nums.size(); i++) {
+            if ( (s.find(nums[i] - diff) != s.end()) && (s.find(nums[i] - (2*diff)) != s.end()) ) {
+                count++;
+            }
+            s.insert(nums[i]);
+        }
+        return count;
+    }
+};
