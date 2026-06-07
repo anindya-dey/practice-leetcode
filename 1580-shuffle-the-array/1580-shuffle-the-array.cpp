8ea6030677e41d195d2e30aef60f86865dc1ff80
Anindya Dey <30517208+anindya-dey@users.noreply.github.com>
2026-06-07 21:59:14 +0530

    1580-SHUFFLE-THE-ARRAY Stats: Time: 9 ms (16.9%), Space: 13.3 MB (79.65%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cpp
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -4,9 +4,14 @@ public:
         int len = n*2;
         vector<int> ans(len);
 
-        for(int i = 0, j = 0; i < n, j < n; i = i + 2, j++) {
-            ans[i] = nums[j];
-            ans[i+1] = nums[j+n];
+        for(int i = 0; i < len; i++) {
+            if (i % 2 == 0) {
+                // for even "i", take from first half
+                ans[i] = nums[i/2];
+            } else {
+                //for odd "i", take from second half
+                ans[i] = nums[n + (i/2)];
+            }
         }
         
         return ans;
